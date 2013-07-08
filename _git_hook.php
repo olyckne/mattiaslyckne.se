b43a5f078cf15f121035f49c7eccb75df5874bb3
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-07-08 16:19:04 +0200

    fixed git hook issue

--- _git_hook.php
+++ _git_hook.php
@@ -1,14 +1,29 @@
 <?php
+$allowed = array(
+	array(
+		'start'	=> '204.232.175.00',
+		'end' 	=> '204.232.175.255'
+	),
+	array(
+		'start' 	=> '192.30.252.0',
+		'end' 	=> '192.30.252.255'
+	)
+);
 
 if(!isset($_REQUEST['payload'])) {
     echo "no no!";
     exit;
 }
-if(in_array($_SERVER['REMOTE_ADDR'], array('204.232.175.64', '192.30.252.0'))) {
-    chdir('..');
-    echo `git fetch --all`;
-    echo `git reset --hard origin/master`;
-    echo `./bootstrap.sh`;
-} else {
-    echo "Not allowed";
+
+$ip = ip2long($_SERVER['REMOTE_ADDR']);
+
+foreach($allowed as $allow) {
+	if( $ip >= ip2long($allow['start'] && $ip <= ip2long($allow['end']))) {
+
+	    chdir('..');
+	    echo `git fetch --all`;
+	    echo `git reset --hard origin/master`;
+	    echo `./bootstrap.sh`;
+	    break;
+	}
 }
