78aeb6a7f394fcc0e1e908c5672bcd6c31f3f7bb
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-07-08 14:27:03 +0200

    tel: link

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ var baseUrl = 'http://localhost:8888/';
 
 var app = angular.module("Olyckne", ['ngResource', 'ui.bootstrap'])
 	.config(	function($compileProvider, $routeProvider, $locationProvider) {
-			$compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|skype):/);
+			$compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|skype|tel):/);
 			$locationProvider.html5Mode(true);
 			$routeProvider
 				.otherwise({redirectTo: '/'});
