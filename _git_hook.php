c149aed1ab2715384ffb9b3d144eb70a5d475b48
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-07-08 15:54:57 +0200

    updated git hook

--- _git_hook.php
+++ _git_hook.php
@@ -1,6 +1,14 @@
 <?php
 
-echo `git fetch --all`;
-echo `git reset --hard origin/master`;
-chdir('..');
-echo `./bootstrap.sh`;
+if(!isset($_REQUEST['payload'])) {
+    echo "no no!";
+    exit;
+}
+if(in_array($_SERVER['REMOTE_ADDR'], array('204.232.175.64', '192.30.252.0'))) {
+    chdir('..');
+    echo `git fetch --all`;
+    echo `git reset --hard origin/master`;
+    echo `./bootstrap.sh`;
+} else {
+    echo "Not allowed";
+}
